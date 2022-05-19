ebdf5324f443033ae1210243013919fe88ae2aad
shop-healthy <97876071+shop-healthy@users.noreply.github.com>
2022-05-19 10:29:57 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -179,7 +179,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=600493857630184,932767647301597,672084573820817';
+            window.location.href = './thankyou.html?fb_pixel_id=425830838912225,530889851577933';
         });
         
     }
